e229a6a12f1929d03872f6e04203eddd679fbcd1
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2024-09-04 14:10:42 +0200

    Camera setup for the boat

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hide the mouse cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
